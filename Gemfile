d598456a00111a84c6065fd930f4dcebd2f432b5
Tom Taylor <tom@tomtaylor.co.uk>
2011-11-01 16:08:49 +0000

    Remove asset pipeline completely.

--- Gemfile
+++ Gemfile
@@ -6,12 +6,6 @@ gem 'jquery-rails'
 gem 'rest-client'
 gem 'stringex'
 
-group :assets do
-  gem 'sass-rails',   '~> 3.1.4'
-  gem 'coffee-rails', '~> 3.1.1'
-  gem 'uglifier', '>= 1.0.3'
-end
-
 group :test do
   gem 'turn', :require => false
   gem 'webmock'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,8 +23,8 @@ Fco::Application.configure do
   config.action_dispatch.best_standards_support = :builtin
 
   # Do not compress assets
-  config.assets.compress = false
+  # config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  # config.assets.debug = true
 end
