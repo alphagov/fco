cffd31aa66de1e965f593ceee3ee25c8b5091776
James Stewart <jys@ketlai.co.uk>
2011-11-04 11:06:28 +0000

    Add jenkins.sh so we can run tests on CI box

---
+++ jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
+bundle exec rake stats
+
+# DELETE STATIC SYMLINKS AND RECONNECT...
+for d in images javascripts templates stylesheets; do
+  rm -f public/$d
+  ln -s ../../../Static/workspace/public/$d public/
+done
+
+export DISPLAY=:99
+bundle exec rake ci:setup:testunit test
+RESULT=$?
+exit $RESULT
