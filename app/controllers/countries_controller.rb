99620a95a936d3ae8479d3d01d4c01d54a071f07
Tom Taylor <tom@tomtaylor.co.uk>
2011-11-07 13:34:11 +0000

    Separate the title and body in the TravelAdviceSectionPresenter.

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,6 +2,9 @@ class CountriesController < ApplicationController
 
   def show
     @country = Country.find_by_slug!(params[:id])
+    @section_presenters = @country.raw_travel_advice['travel_advice_sections'].map do |s|
+      TravelAdviceSectionPresenter.new(s)
+    end
     @missions = @country.missions
   end
 
