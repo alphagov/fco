9c68c83c0a3e97ffa700366277f260146a73e502
Tom Taylor <tom@tomtaylor.co.uk>
2011-11-09 11:48:29 +0000

    All the routes should be prefixed with travel-advice

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Fco::Application.routes.draw do
 
-  root :to => 'home#show'
-  match '/countries/:id', :to => 'countries#show', :as => :country, :via => :get
-  match '/countries/:country_id/missions/:id', :to => 'missions#show', :as => :mission, :via => :get
-  match '/news/:year/:month/:day/:slug',
-    :constraints => {
-      :year => /[0-9]{4}/, :month => /[0-9]{2}/, :day => /[0-9]{2}/
-    },
-    :as => :news_item,
-    :to => 'travel_news#show',
-    :via => :get
+  scope "travel-advice" do
+    root :to => 'home#show'
+    match '/countries/:id', :to => 'countries#show', :as => :country, :via => :get
+    match '/countries/:country_id/missions/:id', :to => 'missions#show', :as => :mission, :via => :get
+    match '/news/:year/:month/:day/:slug',
+      :constraints => {
+        :year => /[0-9]{4}/, :month => /[0-9]{2}/, :day => /[0-9]{2}/
+      },
+      :as => :news_item,
+      :to => 'travel_news#show',
+      :via => :get
+  end
 
 end
